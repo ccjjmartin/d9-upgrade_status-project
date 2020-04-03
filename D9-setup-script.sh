b32f63d289de3b10db1b34eab4f66e4c69f8e0aa
mcdwayne <1dwayne.mcdaniel@gmail.com>
2020-04-03 13:40:33 -0500

    initial commit for passing to vbox

---
+++ D9-setup-script.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+## VERY IMPORTANT STUFF
+##
+## Make sure this is true on ubuntu: 'sudo apt-get install php php-curl php-gd php-mbstring php-sqlite3' 
+## might need another lbrary but I forgot to document as I went...
+##
+## 1. composer installed Drupal 8 with `composer create-project drupal/recommended-project my_site_name_dir`
+## see: https://www.drupal.org/docs/develop/using-composer/using-composer-to-install-drupal-and-manage-dependencies
+##
+## 2. installed and started a server with `php ./core/scripts/drupal quick-start demo_umami`
+## see: https://www.drupal.org/docs/8/install/drupal-8-quick-start-command
+##
+## 3. Make sure I have this .csv in place https://github.com/mcdwayne/d9-drupal-check-allthemodules/blob/master/modulelistmachinenames.csv AND the checkall script in place
+##
+## 4. SUPER VITAL!!!  Fresh installs harden permissions and will not let you rewrite the needed .yml file to let composer work(which is good for prodction, bad for local dev) 
+## run `chmod u+w web/sites/default` to fix this.
+##
+## 5. `composer require drush/drush` (I have drush watcher launcher installed too, makes it easy, 
+## see: https://docs.drush.org/en/master/install/)
+##
+## 6. `composer require drupal/upgrade_status`
+## This is the tool that needs an active running Drupal site from what I could see.  
+##
+## 7. Modify the '/modules/contrib/upgrade_status/src/Commands/UpgradeStatusCommands.php' to enable the `upgrade_status:checkstyle` command
+## I am using the update from Gábor Hojtsy (@goba) as found here: https://git.drupalcode.org/project/upgrade_status/blob/8.x-2.x/src/Commands/UpgradeStatusCommands.php
+## `curl https://git.drupalcode.org/project/upgrade_status/-/raw/8.x-2.x/src/Commands/UpgradeStatusCommands.php > web/modules/contrib/upgrade_status/src/Commands/UpgradeStatusCommands.php ``
+##
+## 8. `drush pm:enable upgrade_status -y`(Super important to make drush upgrade_status:checkstyle work)
+##
+## 9. git init, add, commit and we should be ready to go.. should be...
+## I called my repo 
+
+
+## what should we call this one?
+
+if [ -z "$1" ]; then
+	echo -n "Name your post as a single dash-connected-string, leave off the suffix please:  " && read SITENAME
+else  
+	SITENAME=$1
+fi
+
+
+## Composer install the site
+composer create-project drupal/recommended-project ${SITENAME}
+
+
+## cd
+
+cd ${SITENAME}
+
+php ./web/core/scripts/drupal quick-start minimal
+
+
+echo 'make sure modulelistmachinenames1.csv and script is in place'
+
+cp ~/Documents/modulelistmachinenames1.csv .
+cp ~/Documents/d9upgrade_statuscheckall.sh .
+
+chmod u+w web/sites/default
+
+composer require drush/drush
+composer require drupal/upgrade_status
+
+curl https://git.drupalcode.org/project/upgrade_status/-/raw/8.x-2.x/src/Commands/UpgradeStatusCommands.php > web/modules/contrib/upgrade_status/src/Commands/UpgradeStatusCommands.php
+
+
+
+drush pm:enable upgrade_status -y
+
+git init
+git add .
+git rm --cached web/modules/contrib/git_deploy
+git commit -m 'initial commit'
+
+echo 'shold be set up right??  Check that csv'
---
+++ d9upgrade_statuscheckall.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+## Point of this script is to utilize drush upgrade_status:checkstyle
+
+## VERY IMPORTANT STUFF
+## 1. composer installed Drupal 8 with `composer create-project drupal/recommended-project my_site_name_dir`
+## see: https://www.drupal.org/docs/develop/using-composer/using-composer-to-install-drupal-and-manage-dependencies
+##
+## 2. installed and started a server with `php ./core/scripts/drupal quick-start demo_umami`
+## see: https://www.drupal.org/docs/8/install/drupal-8-quick-start-command
+##
+## 3. Make sure I have this .csv in place https://github.com/mcdwayne/d9-drupal-check-allthemodules/blob/master/modulelistmachinenames.csv
+##
+## 4. SUPER VITAL!!!  Fresh installs harden permissions and will not let you rewrite the needed .yml file to let composer work(which is good for prodction, bad for local dev) 
+## run `chmod u+w web/sites/default` to fix this.
+##
+## 5. `composer require drush/drush` (I have drush watcher launcher installed too, makes it easy, 
+## see: https://docs.drush.org/en/master/install/)
+##
+## 6. `composer require drupal/upgrade_status`
+## This is the tool that needs an active running Drupal site from what I could see.  
+##
+## 7. Modify the '/modules/contrib/upgrade_status/src/Commands/UpgradeStatusCommands.php' to enable the `upgrade_status:checkstyle` command
+## I am using the update from Gábor Hojtsy (@goba) as found here: https://git.drupalcode.org/project/upgrade_status/blob/8.x-2.x/src/Commands/UpgradeStatusCommands.php
+## `curl https://git.drupalcode.org/project/upgrade_status/-/raw/8.x-2.x/src/Commands/UpgradeStatusCommands.php > web/modules/contrib/upgrade_status/src/Commands/UpgradeStatusCommands.php ``
+##
+## 8. `drush pm:enable upgrade_status -y`(Super important to make drush upgrade_status:checkstyle work)
+##
+## 9. git init, add, commit and we should be ready to go.. should be...
+## I called my repo 
+
+# cd ~/Documents/d9-ready # make sure the folder is the name of the folder with composer.son above /web
+git checkout master # just make sure.
+
+## Open the csv of the list of allllllll the modules
+INPUT=modulelistmachinenames1.csv
+
+##Safe way to deal with Internal File Separator or IFS resets
+OLDIFS=$IFS
+IFS=,
+
+## Error if fine not found
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+
+## Loop through the CSV
+while read col1 col2
+do
+## threw errors if I didn't store this one as a variable, not sure why.  
+## Store the machine name as a variable
+MODULENAME=$col1
+
+## Print to screen just for keeping an eye on progress reasons. 
+echo $MODULENAME
+
+git checkout -b $MODULENAME
+
+# Install the module with composer
+composer require ${col2}
+
+# YOU MUST ENABLE THE MODULE TO MAKE upgrade_status:checkstyle work
+drush pm:enable $MODULENAME -y
+
+
+## Push to a .xml file
+drush upgrade_status:checkstyle ${MODULENAME} > ../reports/${MODULENAME}.xml 
+
+# grep the hash and push it to the bottom of the file...Wait, should I be doing this? Would it matter?
+# composer show -i drupal/${MODULENAME} | grep source >> ../reports/${MODULENAME}.xml
+
+# clean up config
+drush pm:uninstall $MODULENAME -y
+
+composer remove ${col2}
+
+# Cheap way out of dependency hell or awaiting composer remove, just reset back to the last commit.  
+git reset --hard
+git add .
+git commit -m 'what commit?'
+git checkout master
+
+## Done with the loop
+done < $INPUT
+
+## reset to the original IFS
+IFS=$OLDIFS
